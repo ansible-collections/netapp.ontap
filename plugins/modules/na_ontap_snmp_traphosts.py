623ed4971dff1c50ec356a65ad2657657be6aa05
Chris Archibald <carchi8py@gmail.com>
2020-03-25 12:45:43 -0400

    Sync bitbucket and Github

---
+++ plugins/modules/na_ontap_snmp_traphosts.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+"""
+create SNMP module to add/delete/modify SNMP user
+"""
+
+# (c) 2020, NetApp, Inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'certified'}
+
+
+DOCUMENTATION = '''
+module: na_ontap_snmp_traphosts
+short_description: NetApp ONTAP SNMP traphosts.
+extends_documentation_fragment:
+    - netapp.ontap.netapp.na_ontap
+version_added: '20.3.0'
+author: NetApp Ansible Team (@carchi8py) <ng-ansibleteam@netapp.com>
+description:
+- Whether the specified SNMP traphost should exist or not. Requires REST with 9.6 or higher
+options:
+  ip_address:
+    description:
+      - "The IP address of the SNMP traphost to manage."
+    required: true
+    type: str
+  state:
+    choices: ['present', 'absent']
+    description:
+      - "Whether the specified SNMP traphost should exist or not."
+    default: 'present'
+    type: str
+'''
+
+EXAMPLES = """
+    - name: Create SNMP traphost
+      na_ontap_snmp:
+        state: present
+        ip_address: '10.10.10.10'
+        hostname: "{{ hostname }}"
+        username: "{{ username }}"
+        password: "{{ password }}"
+    - name: Delete SNMP traphost
+      na_ontap_snmp:
+        state: absent
+        ip_address: '10.10.10.10'
+        hostname: "{{ hostname }}"
+        username: "{{ username }}"
+        password: "{{ password }}"
+"""
+
+RETURN = """
+"""
+from ansible.module_utils.basic import AnsibleModule
+import ansible_collections.netapp.ontap.plugins.module_utils.netapp as netapp_utils
+from ansible_collections.netapp.ontap.plugins.module_utils.netapp_module import NetAppModule
+from ansible_collections.netapp.ontap.plugins.module_utils.netapp import OntapRestAPI
+
+
+class NetAppONTAPSnmpTraphosts(object):
+    """Class with SNMP methods"""
+
+    def __init__(self):
+        self.use_rest = False
+        self.argument_spec = netapp_utils.na_ontap_host_argument_spec()
+        self.argument_spec.update(dict(
+            state=dict(required=False, type='str', choices=['present', 'absent'], default='present'),
+            ip_address=dict(required=True, type='str'),
+        ))
+        self.module = AnsibleModule(
+            argument_spec=self.argument_spec,
+            supports_check_mode=True
+        )
+        self.na_helper = NetAppModule()
+        self.parameters = self.na_helper.set_parameters(self.module.params)
+        self.restApi = OntapRestAPI(self.module)
+        if not self.restApi.is_rest():
+            self.module.fail_json(msg="na_ontap_snmp_traphosts only support Rest and ONTAP 9.6+")
+
+    def get_snmp_traphosts(self):
+        params = {'ip_address': self.parameters['ip_address']}
+        api = 'support/snmp/traphosts'
+        message, error = self.restApi.get(api, params)
+        if error:
+            self.module.fail_json(msg=error)
+        if not message['records']:
+            return None
+        return message['records']
+
+    def create_snmp_traphost(self):
+        api = '/support/snmp/traphosts'
+        params = {'host': self.parameters['ip_address']}
+        message, error = self.restApi.post(api, params)
+        if error:
+            self.module.fail_json(msg=error)
+
+    def delete_snmp_traphost(self):
+        api = '/support/snmp/traphosts/' + self.parameters['ip_address']
+        params = None
+        message, error = self.restApi.delete(api, params)
+        if error is not None:
+            self.module.fail_json(msg="Error deleting traphost: %s" % error)
+
+    def apply(self):
+        """
+        Apply action to SNMP traphost
+        """
+        current = self.get_snmp_traphosts()
+        cd_action = self.na_helper.get_cd_action(current, self.parameters)
+        if self.na_helper.changed:
+            if self.module.check_mode:
+                pass
+            else:
+                if cd_action == 'create':
+                    self.create_snmp_traphost()
+                elif cd_action == 'delete':
+                    self.delete_snmp_traphost()
+
+        self.module.exit_json(changed=self.na_helper.changed)
+
+
+def main():
+    """Execute action"""
+    community_obj = NetAppONTAPSnmpTraphosts()
+    community_obj.apply()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/unit/plugins/modules/test_na_ontap_snmp_traphosts.py
@@ -0,0 +1,152 @@
+# (c) 2020, NetApp, Inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+""" unit tests for Ansible module: na_ontap_snmp_traphosts """
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+import json
+import pytest
+
+from ansible_collections.netapp.ontap.tests.unit.compat import unittest
+from ansible_collections.netapp.ontap.tests.unit.compat.mock import patch, Mock
+from ansible.module_utils import basic
+from ansible.module_utils._text import to_bytes
+
+from ansible_collections.netapp.ontap.plugins.modules.na_ontap_snmp_traphosts \
+    import NetAppONTAPSnmpTraphosts as traphost_module  # module under test
+
+# REST API canned responses when mocking send_request
+
+SRR = {
+    # common responses
+    'is_rest': (200, {}, None),
+    'is_zapi': (400, {}, "Unreachable"),
+    'empty_good': ({}, None),
+    'end_of_sequence': (None, "Unexpected call to send_request"),
+    'generic_error': (None, "Expected error"),
+    # module specific responses
+    'no_record': ({"records": {}}, None),
+    'get_snmp_traphosts': (
+        {"records": [{
+            "host": "0.0.0.0",
+            "ip_address": "0.0.0.0"
+        }]
+        }, None
+    )
+}
+
+def set_module_args(args):
+    """prepare arguments so that they will be picked up during module creation"""
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)  # pylint: disable=protected-access
+
+
+class AnsibleExitJson(Exception):
+    """Exception class to be raised by module.exit_json and caught by the test case"""
+    pass
+
+
+class AnsibleFailJson(Exception):
+    """Exception class to be raised by module.fail_json and caught by the test case"""
+    pass
+
+
+def exit_json(*args, **kwargs):  # pylint: disable=unused-argument
+    """function to patch over exit_json; package return data into an exception"""
+    if 'changed' not in kwargs:
+        kwargs['changed'] = False
+    raise AnsibleExitJson(kwargs)
+
+
+def fail_json(*args, **kwargs):  # pylint: disable=unused-argument
+    """function to patch over fail_json; package return data into an exception"""
+    kwargs['failed'] = True
+    raise AnsibleFailJson(kwargs)
+
+class TestMyModule(unittest.TestCase):
+    """ Unit tests for na_ontap_wwpn_alias """
+
+    def setUp(self):
+        self.mock_module_helper = patch.multiple(basic.AnsibleModule,
+                                                 exit_json=exit_json,
+                                                 fail_json=fail_json)
+        self.mock_module_helper.start()
+        self.addCleanup(self.mock_module_helper.stop)
+        self.mock_alias = {
+            'ip_address': '0.0.0.0',
+        }
+
+    def mock_args(self):
+        return {
+            'ip_address': self.mock_alias['ip_address'],
+            'hostname': 'test_host',
+            'username': 'test_user',
+            'password': 'test_pass!'
+        }
+
+    def get_alias_mock_object(self):
+        alias_obj = traphost_module()
+        return alias_obj
+
+    @patch('ansible_collections.netapp.ontap.plugins.module_utils.netapp.OntapRestAPI.send_request')
+    def test_rest_successful_create(self, mock_request):
+        """Test successful rest create"""
+        data = self.mock_args()
+        set_module_args(data)
+        mock_request.side_effect = [
+            SRR['is_rest'],
+            SRR['no_record'],
+            SRR['empty_good'],
+            SRR['end_of_sequence']
+        ]
+        with pytest.raises(AnsibleExitJson) as exc:
+            self.get_alias_mock_object().apply()
+        assert exc.value.args[0]['changed']
+
+    @patch('ansible_collections.netapp.ontap.plugins.module_utils.netapp.OntapRestAPI.send_request')
+    def test_rest_create_idempotency(self, mock_request):
+        """Test rest create idempotency"""
+        data = self.mock_args()
+        set_module_args(data)
+        mock_request.side_effect = [
+            SRR['is_rest'],
+            SRR['get_snmp_traphosts'],
+            SRR['empty_good'],
+            SRR['end_of_sequence']
+        ]
+        with pytest.raises(AnsibleExitJson) as exc:
+            self.get_alias_mock_object().apply()
+        assert not exc.value.args[0]['changed']
+
+    @patch('ansible_collections.netapp.ontap.plugins.module_utils.netapp.OntapRestAPI.send_request')
+    def test_rest_successful_delete(self, mock_request):
+        """Test successful rest delete"""
+        data = self.mock_args()
+        data['state'] = 'absent'
+        set_module_args(data)
+        mock_request.side_effect = [
+            SRR['is_rest'],
+            SRR['get_snmp_traphosts'],
+            SRR['empty_good'],
+            SRR['end_of_sequence']
+        ]
+        with pytest.raises(AnsibleExitJson) as exc:
+            self.get_alias_mock_object().apply()
+        assert exc.value.args[0]['changed']
+
+    @patch('ansible_collections.netapp.ontap.plugins.module_utils.netapp.OntapRestAPI.send_request')
+    def test_rest_delete_idempotency(self, mock_request):
+        """Test successful rest delete"""
+        data = self.mock_args()
+        data['state'] = 'absent'
+        set_module_args(data)
+        mock_request.side_effect = [
+            SRR['is_rest'],
+            SRR['no_record'],
+            SRR['empty_good'],
+            SRR['end_of_sequence']
+        ]
+        with pytest.raises(AnsibleExitJson) as exc:
+            self.get_alias_mock_object().apply()
+        assert not exc.value.args[0]['changed']
