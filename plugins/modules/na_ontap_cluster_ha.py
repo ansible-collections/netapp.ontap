a84fe4c3de78f4ba908f12fc7eac47f5d6182a14
Chris Archibald <carchi8py@gmail.com>
2020-04-16 12:37:27 -0400

    Sync bitbucket and Github

--- plugins/modules/na_ontap_cluster_ha.py
+++ plugins/modules/na_ontap_cluster_ha.py
@@ -113,10 +113,11 @@ class NetAppOntapClusterHA(object):
         netapp_utils.ems_log_event("na_ontap_cluster_ha", cserver)
         current = self.get_cluster_ha_enabled()
         cd_action = self.na_helper.get_cd_action(current, self.parameters)
-        if cd_action == 'create':
-            self.modify_cluster_ha("true")
-        elif cd_action == 'delete':
-            self.modify_cluster_ha("false")
+        if not self.module.check_mode:
+            if cd_action == 'create':
+                self.modify_cluster_ha("true")
+            elif cd_action == 'delete':
+                self.modify_cluster_ha("false")
 
         self.module.exit_json(changed=self.na_helper.changed)
 
