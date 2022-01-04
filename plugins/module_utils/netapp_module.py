8f41ff0ccdfe98ad2db5f07bc467a4a6a2efd85f
Chris Archibald <carchi8py@gmail.com>
2022-01-04 16:25:22 -0500

    Sync bitbucket and GitHub

--- plugins/module_utils/netapp_module.py
+++ plugins/module_utils/netapp_module.py
@@ -189,22 +189,6 @@ class NetAppModule(object):
             return 'delete'
         return 'create'
 
-    def compare_and_update_values(self, current, desired, keys_to_compare):
-        updated_values = {}
-        is_changed = False
-        for key in keys_to_compare:
-            if key in current:
-                if key in desired and desired[key] is not None:
-                    if current[key] != desired[key]:
-                        updated_values[key] = desired[key]
-                        is_changed = True
-                    else:
-                        updated_values[key] = current[key]
-                else:
-                    updated_values[key] = current[key]
-
-        return updated_values, is_changed
-
     @staticmethod
     def check_keys(current, desired):
         ''' TODO: raise an error if keys do not match
