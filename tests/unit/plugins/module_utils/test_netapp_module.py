19ddc53d6dc0d1e2c45e40792de435fd83e0ebe2
Chris Archibald <carchi8py@gmail.com>
2022-08-30 14:47:56 -0400

    Sync bitbucket and GitHub

--- tests/unit/plugins/module_utils/test_netapp_module.py
+++ tests/unit/plugins/module_utils/test_netapp_module.py
@@ -558,12 +558,16 @@ def test_safe_get_with_exception():
     assert 'c' == error
     # IndexError
     error = expect_and_capture_ansible_exception(my_obj.na_helper.safe_get, IndexError, get_zapi_info(), ['a', 'bad_stuff', 4], allow_sparse_dict=False)
-    if sys.version_info > (3, 5, 9):
+    print(str(error))
+    if sys.version_info > (3, 6, 8):
         # this fails on 3.5.7 but works on 3.5.10
+        # this fails on 3.6.8 but works on 3.6.9
         assert 'list index out of range' in str(error)
     error = expect_and_capture_ansible_exception(my_obj.na_helper.safe_get, IndexError, get_zapi_info(), ['a', 'bad_stuff', -4], allow_sparse_dict=False)
-    if sys.version_info > (3, 5, 9):
+    print(str(error))
+    if sys.version_info > (3, 6, 8):
         # this fails on 3.5.7 but works on 3.5.10
+        # this fails on 3.6.8 but works on 3.6.9
         assert 'list index out of range' in str(error)
     # TypeError - not sure I can build a valid ZAPI NaElement that can give a type error, but using a dict worked.
     error = expect_and_capture_ansible_exception(my_obj.na_helper.safe_get, TypeError, get_zapi_info(), ['a', 'bad_stuff', 'extra'], allow_sparse_dict=False)
